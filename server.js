79389c858b1cea7ab1ab89385b9f143beecb79ff
CosmicTiger <luisangelmarcia@gmail.com>
2021-02-25 19:28:58 -0600

    :sparkles: - @CosmicTiger - Done endpoints

--- server.js
+++ server.js
@@ -6,6 +6,41 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/book', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'It was process correctly'
+    });
+});
+
+app.get('/api/book/:id', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Book by Id was returned'
+    });
+});
+
+app.post('/api/book/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Book was created correctly'
+    });
+});
+
+app.put('/api/book/:id', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Book was updated correctly'
+    });
+});
+
+app.delete('/api/book/:id', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Book was deleted correctly'
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log('Server is executing in environment', process.env.NODE_ENV));
